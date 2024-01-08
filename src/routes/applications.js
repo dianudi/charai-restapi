30908c054857b69f7278d102e7d05db44be4bedd
dianudi <144350056+dianudi@users.noreply.github.com>
2024-01-08 21:11:59 +0700

    add application docs

--- src/routes/applications.js
+++ src/routes/applications.js
@@ -143,6 +143,13 @@ const r = Router();
  *       - Application
  *     security:
  *       - BearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: Application ID
+ *         required: true
+ *         schema:
+ *           type: integer
  *     responses:
  *       200:
  *         description: Return consume application data.
@@ -151,10 +158,185 @@ const r = Router();
  *             schema:
  *               type: object
  *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                     type: integer
+ *                     description: Unique ID
+ *                     example: 1
+ *                     name:
+ *                       type: string
+ *                       description: Name of consume application
+ *                       example: My Assistant
+ *                     access_token:
+ *                       type: string
+ *                       description: Access token for authorizing
+ *                       example: blablabla
+ *                     mutiple_conversation:
+ *                       type: boolean
+ *                       description: Can consume application make more conversation
+ *                       example: true
+ *                     created_at:
+ *                       type: string
+ *                       description: Created at
+ *                     updated_at:
+ *                       type: string
+ *                       description: Updated at
  *                 
  *       404:
  *         description: Not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: Not Found
+ * 
+ *   patch:
+ *     description: Update consume application
+ *     summary: Update consume application
+ *     tags:
+ *       - Application
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: Application ID
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       description: Required parameters to send.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Name of consume application.
+ *                 example: My Assistant
+ *               description:
+ *                 type: string
+ *                 description: Description of consume application.
+ *                 example: Lorem ipsum dolor sit amet
+ *               multiple_conversation:
+ *                 type: boolean
+ *                 description: Can consume application make more conversation.
+ *     responses:
+ *       201:
+ *         description: Success updated new consume application
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: Updated
+
+ *       422:
+ *         description: Validation failed.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: array
+ *                   description: Errors field.
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       location:
+ *                         type: string
+ *                         description: Location of error.
+ *                         example: body
+ *                       msg:
+ *                         type: string
+ *                         description: Message of error.
+ *                         example: Name can't empty
+ *                       path:
+ *                         type: string
+ *                         description: Field of error
+ *                         example: name
+ *                       type:
+ *                         type: string
+ *                         description: Type
+ *                         example: field
  *
+ *
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: Unauthorized
+ *       404:
+ *         description: Not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: Not Found
+ * 
+ *   delete:
+ *     description: Delete consume application
+ *     summary: Delete consume application
+ *     tags:
+ *       - Application
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: Application ID
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Success deleted consume application
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: Deleted
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: Unauthorized
+  *       404:
+ *         description: Not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: Not Found
+ * 
  */
 r.use("/api/applications", auth);
 r.route("/api/applications").get(index).post(storeUpdateApplicationRequest, store);
--- src/routes/characters.js
+++ src/routes/characters.js
@@ -4,6 +4,67 @@ import { auth } from "../middlewares/auth.js";
 import { storeCharacterRequest, updateCharacterRequest } from "../requests/characterRequest.js";
 
 const r = Router();
+/**
+ * @swagger
+ * /api/characters:
+ *   get:
+ *     description: Get all AI characters.
+ *     summary: Ger all AI characters.
+ *     tags:
+ *       - Character
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Return all character.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: Unique ID.
+ *                     exampe: 1
+ *                   name:
+ *                     type: string
+ *                     description: Character name.
+ *                     example: Lorem Ipsum
+ *                   short_description:
+ *                     type: string
+ *                     description: Short description of AI character.
+ *                     example: Lorem ipsum dolor sit amet.
+ *                   description:
+ *                     type: string
+ *                     description: Long description of AI character.
+ *                     example: Lorem ipsum dolor sit amet.
+ *                   char_id:
+ *                     type: string
+ *                     description: Character id from character.ai.
+ *                     example: randomcharacterstring
+ *                   gender:
+ *                     type: string
+ *                     description: Gender of AI character.
+ *                     example: male
+ *                   created_at:
+ *                     type: string
+ *                     description: Created at.
+ *                   updated_at:
+ *                     type: string
+ *                     description: Updated at.
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: Unauthorized
+ */
 r.use("/api/characters", auth);
 r.route("/api/characters").get(index).post(storeCharacterRequest, store);
 r.route("/api/characters/:id").get(show).patch(updateCharacterRequest, update).delete(destroy);
