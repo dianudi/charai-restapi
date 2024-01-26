08f55b92391d963cabc26ceb751fade1cfc800a8
dianudi <144350056+dianudi@users.noreply.github.com>
2024-01-26 22:32:04 +0700

    add credential update route

---
+++ src/routes/credential.js
@@ -0,0 +1,86 @@
+import { Router } from "express";
+import { auth } from "../middlewares/auth.js";
+
+const r = Router();
+/**
+ * @swagger
+ * /api/credential:
+ *   get:
+ *     description: Update Character AI token for authorized action.
+ *     summary: Update Character AI token.
+ *     tags:
+ *       - Credential Token
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       description: Required parameters to send.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               access_token:
+ *                 type: string
+ *                 description: Access token from local storage.
+ *                 example: randomstring
+ *               id_token:
+ *                 type: string
+ *                 description: ID Token from local storage
+ *                 example: randomstring
+ *     responses:
+ *       200:
+ *         description: Return success update token.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   description: Message
+ *                   example: Success update token
+ *
+ *       401:
+ *         description: Unauthorized action.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: Unauthorized
+ *       422:
+ *         description: Validation failed.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: array
+ *                   description: Errors field.
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       location:
+ *                         type: string
+ *                         description: Location of error.
+ *                         example: body
+ *                       msg:
+ *                         type: string
+ *                         description: Message of error.
+ *                         example: Name can't empty
+ *                       path:
+ *                         type: string
+ *                         description: Field of error
+ *                         example: name
+ *                       type:
+ *                         type: string
+ *                         description: Type
+ *                         example: field
+ *
+ */
+r.use("/api/credential", auth);
+r.route("/api/credential").patch();
+export default r;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,5 @@ import char from "./characters.js";
 import auth from "./auth.js";
 import app from "./applications.js";
 import chat from "./chat.js";
-export default [auth, char, app, chat];
+import credential from "./credential.js";
+export default [auth, char, app, chat, credential];
